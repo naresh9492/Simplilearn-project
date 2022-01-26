d9d5820e125c40b51c0266af4202eda5412807ff
Naresh9492 <97113894+naresh9492@users.noreply.github.com>
2022-01-26 12:03:10 +0530

    added

--- Phase1Projects/Assisted-and-practice projects/Acessmodifer.java
+++ Phase1Projects/Assisted-and-practice projects/Acessmodifer.java
@@ -1,11 +1,11 @@
 package project;
 
 public class Acessmodifer {
-	public int a=10;
+public int a=10;
 private int b=20;
 int c=30;
 protected int d=40;
-public void hi(){
+public void hello(){
 System.out.print(a);
 System.out.print(b);
 System.out.print(c);
@@ -17,8 +17,8 @@ System.out.print(d);
 class j{
 	public static void main(String[] args)
 	{
-		Acessmodifer o=new Acessmodifer();
-		o.hi();
+		Acessmodifer out=new Acessmodifer();
+		out.hello();
 	}
 }
 
