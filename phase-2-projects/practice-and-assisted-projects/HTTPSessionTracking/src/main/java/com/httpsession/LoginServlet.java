30306b4edaf7bfaf529db5b5d272fde5dd7a75fe
Naresh9492 <97113894+naresh9492@users.noreply.github.com>
2022-01-29 15:14:55 +0530

    Added HttpSession

---
+++ phase-2-projects/practice-and-assisted-projects/HTTPSessionTracking/src/main/java/com/httpsession/LoginServlet.java
@@ -0,0 +1,45 @@
+package com.httpsession;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	String userid = request.getParameter("userid");
+	HttpSession session = request.getSession();
+	session.setAttribute(userid, "userid");
+	
+	response.sendRedirect("Dashboard");
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
