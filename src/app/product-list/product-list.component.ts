dbef43a1339645cc4c6b9ecfc74c455a783decd3
naresh9492 <nareshkuruba1998@gmail.com>
2022-02-25 20:23:56 +0530

    Added property binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
   pageTitle: string = "Product List Page"
+  src = 'https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200-1.png';
 
   constructor() { }
 
